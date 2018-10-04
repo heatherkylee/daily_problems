5a55f15d4c8572cafa597de04dbfa63477e6c61a
Heather Lee <heather.k.lee@gmail.com>
2018-10-03 22:32:08 -0400

    complete scabble daily problem

--- 071818.rb
+++ 071818.rb
@@ -4,14 +4,25 @@
 
 def scrabble_score(input)
   points = {'a'=> 1, 'b'=> 3, 'c'=> 3, 'd'=> 2, 'e'=> 1, 'f'=> 4, 'g'=> 2, 'h'=> 4, 'i'=> 1, 'j'=> 8, 'k'=> 5, 'l'=> 1, 'm'=> 3, 'n'=> 1, 'o'=> 1, 'p'=> 3, 'q'=> 10, 'r'=> 1, 's'=> 1, 't'=> 1, 'u'=> 1, 'v'=> 4, 'w'=> 4, 'x'=> 8, 'y'=> 4, 'z'=> 10}
-  i = 0
-  word = input.chars
-  input.length do
-    
-    i += 0    
-  end
+  points.fetch(input)
 end
+
+p scrabble_score('z')
 
 
 # Use the `reduce` method to perform the computation in your scrabble_score method. The code should work as follows:
 # ```p scrabble_score("scholar")  #=> 12```
+
+def scrabble_score(input)
+  points = {'a'=> 1, 'b'=> 3, 'c'=> 3, 'd'=> 2, 'e'=> 1, 'f'=> 4, 'g'=> 2, 'h'=> 4, 'i'=> 1, 'j'=> 8, 'k'=> 5, 'l'=> 1, 'm'=> 3, 'n'=> 1, 'o'=> 1, 'p'=> 3, 'q'=> 10, 'r'=> 1, 's'=> 1, 't'=> 1, 'u'=> 1, 'v'=> 4, 'w'=> 4, 'x'=> 8, 'y'=> 4, 'z'=> 10}
+  word = input.split('')
+  point_value = []
+  index = 0
+  input.length.times do
+    point_value << points.fetch(word[index])
+    index += 1
+  end
+  p point_value.reduce(:+)
+end
+
+p scrabble_score('zoo')
