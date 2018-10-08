2d2d45cee296ffcfd3127bb34ea7bbab291bc884
Heather Lee <heather.k.lee@gmail.com>
2018-10-07 20:18:26 -0400

    daily problem using map, select, and reduce methods

---
+++ 100718.rb
@@ -0,0 +1,17 @@
+# Use the methods `map`, `select`, and `reduce` with the following array of hashes: 
+# ```
+items = [
+  {title: "NoDBA", words: 561, tags: ["nosql", "people", "orm"], type: :book},
+  {title: "Infodeck", words: 1145, tags: ["nosql", "writing"], type: :book},
+  {title: "OrmHate", words: 1718, tags: ["nosql", "orm"], type: :book},
+  {title: "ruby", words: 1313, tags: ["ruby"], type: :article},
+  {title: "DDD_Aggregate", words: 482, tags: ["nosql", "ddd"], type: :book}
+]
+# ```
+# and calculate the total amount of words for all items of type `:book`. The result should be
+# ```
+# 3906
+# ```
+
+number_of_words = items.select { |x| x[:type] == :book }.map  { |y| y[:words] }.reduce(:+)
+p number_of_words
